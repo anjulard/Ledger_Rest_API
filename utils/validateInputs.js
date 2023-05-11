def36d4c0ea62b331b077d48eeea91e59024585f
anjulard <ridmianjula@gmail.com>
2023-05-11 22:50:49 +0530

    Added input validations for date and weekly rent

--- utils/validateInputs.js
+++ utils/validateInputs.js
@@ -1,13 +1,28 @@
+import moment from 'moment';
 
 export const validateInputs = (input) => {
     const validInputs = ["start_date", "end_date", "frequency", "weekly_rent"];
-    console.log(input);
     let errors = [];
-    for (const element of validInputs) {
+    for (let element of validInputs) {
         if(! Object.keys(input).includes(element)) {
             errors.push(`${element} is required field`);
+            return errors;
         }
     }
-      return errors;
+    // Check date validation 
+    if (! (moment(input.start_date, "YYYY-MM-DDTHH:mm:ss", true).isValid() && 
+                                        moment(input.end_date, "YYYY-MM-DDTHH:mm:ss", true).isValid())) {
+        errors.push('Invalid date format of date');
+        return errors;
+            
+    }
+
+    // Check number validation for weekly rent
+    if (! Number.isFinite(input.weekly_rent)) { 
+        errors.push('Invalid number format for Weekly Rent');
+        return errors;
+    }
+
+    return errors;
     
   }
