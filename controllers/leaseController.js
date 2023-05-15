1fa4899fd9902f73db70215435799d5d6290aa85
anjulard <ridmianjula@gmail.com>
2023-05-15 23:19:14 +0530

    Modified input validation in Post Request

--- controllers/leaseController.js
+++ controllers/leaseController.js
@@ -1,24 +1,24 @@
 import { v4 as uuidv4 } from 'uuid';
-import { validateInputs } from "../utils/validateInputs.js";
+import { validationResult } from "express-validator";
 
 // Data array is empty at start
 let leases = [];
 
 // POST Route
 export const createLease = (req, res) => {
-    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+    });
+}
     const lease = req.body;
-    const errors = validateInputs(lease);
-    if (errors.length > 0) {
-        return res.status(400).json({ errors: errors });
-    
-    }else {
+    //const errors = validateInputs(lease);
         let leaseId = uuidv4();
         leases.push({...lease, lease_id: leaseId });
         console.log(lease);
-        res.send(`Lease ${leaseId} is added successfully.`);
-    }
-     
+        res.send(`New lease with Lease Id : ${leaseId} is added successfully.`);
+
 }
 
 // GET all lease
--- routes/ledger.js
+++ routes/ledger.js
@@ -1,15 +1,42 @@
-import express from "express";
+import express                    from "express";
 import { getLease, createLease  } from "../controllers/leaseController.js";
 import { fetchLedger }            from "../controllers/ledgerController.js";
+import { body }                   from "express-validator";
 
 // initialize router
 const router = express.Router();
 
 // Create a new Lease
-router.post('/', createLease);
+router.post('/', [
+    body('start_date')
+        .notEmpty()
+        .withMessage('Start Date is required.')
+        .isISO8601()
+        .withMessage('Invalid start_date. Please add start_date in ISO Date Format.'),
+
+    body('end_date')
+        .notEmpty()
+        .withMessage('End Date is required.')
+        .isISO8601()
+        .withMessage('Invalid end_date. Please add end_date in ISO Date Format.'),
+
+    body('weekly_rent')
+        .notEmpty()
+        .withMessage('Weekly Rent is required')
+        .isNumeric()
+        .withMessage('Invalid weekly rent value. Please add weekly_rent in number format.'),
+    body('frequency')
+        .notEmpty()
+        .withMessage('Frequency is required')
+        .isIn(['WEEKLY', 'FORTNIGHTLY', 'MONTHLY'])
+        .withMessage('Invalid frequency value. Frequency value should be WEEKLY, FORTNIGHTLY or MONTHLY'),
+
+], createLease);
+
 
 // Get all available Lease
 router.get('/', getLease);
+
 
 // In index.js '/ledger' is used as the starting path. All routes starting with '/ledger' here.
 router.get('/generateLedger/:lease_id', fetchLedger);
