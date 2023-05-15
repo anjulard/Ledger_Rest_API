fa62c3189555e086354b7b4308fbb0a8f75bda8c
anjulard <ridmianjula@gmail.com>
2023-05-15 18:01:10 +0530

    Modified Line Item Start Dates and End Dates

--- services/ledgerServices.js
+++ services/ledgerServices.js
@@ -1,36 +1,38 @@
 import moment from 'moment';
 
 
-// Create ledger
+// Create Ledger
 export const generateLedger = (start_date, end_date, frequency, weekly_rent) => {
+    let paymentLines = [];
     let lineItem     = [];
-    let numberOfDays ;
-    let tempDate ;
     let amount       = getAmount(weekly_rent, frequency);
     end_date         = moment(end_date);
-    for (let index = moment(start_date) ; index.isBefore(end_date); index.add(numberOfDays + 1, 'd')) {
-        numberOfDays  = getFrequencyData(frequency, index);
-        let startDate = moment(index);
-        tempDate      = moment(startDate).add(numberOfDays, 'd');
-            
+
+    if ( frequency == "WEEKLY" || frequency === "FORTNIGHTLY") {
+        lineItem = getWeeklyPaymentDates( start_date, end_date, frequency );
+    } else if ( frequency === "MONTHLY") {
+        lineItem = getMonthlyPaymentDates( start_date, end_date );
+    }
+    
+    for (let index in lineItem) {
+
         // Calculate amount to be paid for a line item that is cut short because of the end date.
-        if ( end_date.isBefore(tempDate)) {
-            tempDate            = tempDate.subtract(numberOfDays, 'd');
+        if ( end_date.isBefore(lineItem[index].temp)) {
+            let tempDate        = moment(lineItem[index - 1].temp).add(1, 'd');
             let remainingdays   = getRemainingDays(end_date, tempDate);
             let remainingAmount = getRemainingAmount(weekly_rent, remainingdays);
 
-            lineItem.push( { tempDate, end_date, remainingAmount } );
+            paymentLines.push( { "Start Date": tempDate, "End Date": end_date, "Amount": remainingAmount } );
 
             break;
     
         }
-
-        lineItem.push( { startDate, tempDate, amount } );
-        numberOfDays = getFrequencyData(frequency, index);
+        
+        paymentLines.push( { "Start Date" : lineItem[index].startDate, "End Date" : lineItem[index].temp, "Amount": amount } );
             
     }      
         
-    return lineItem;
+    return paymentLines;
     
 }
 
@@ -44,15 +46,6 @@ export const getFrequencyData = (frequency, start_date) => {
        
     }else if (frequency === "FORTNIGHTLY") {
         numberOfDays = 14;
-    }
-    else if (frequency === "MONTHLY") {
-        let next_date = getNextMonthDate(start_date);
-
-        if (next_date instanceof Date) {
-            numberOfDays = (validateDate(next_date).getTime() - validateDate(start_date).getTime())/ (1000 * 60 * 60 * 24);
-            numberOfDays = Math.ceil(numberOfDays);
-        }
-        
     }
 
     return numberOfDays;
@@ -108,16 +101,17 @@ export const validateDate = (date) => {
     return new Date(date);
 }
 
-// Get same day every month for a given date. Get the closest day in the month if the start date of a line item 
-// doesn’t exist in some months.
 
-export const getMonthlyDate = (start, end) => {
-    let line = [];
+/* Get monthly payment dates as an object array. Get same day every month for a given date. Get the closest day
+in the month if the start date of a line item  doesn’t exist in some months. */
+
+export const getMonthlyPaymentDates = (start, end) => {
     let temp;
+    let line        = [];
     let numofmonths = 1;
-    let startDate = moment(start);
-    let nextdate = moment(start);
-    for (let index = moment(start) ; index.isBefore(end); index.add(1, 'M')) {
+    let startDate   = moment(start);
+    let nextdate    = moment(start);
+    for (let index  = moment(start) ; index.isBefore(end); index.add(1, 'M')) {
         
         nextdate = start;
         moment(nextdate).add(numofmonths, 'M');
@@ -126,6 +120,7 @@ export const getMonthlyDate = (start, end) => {
             temp = new Date(moment(nextdate).add(numofmonths, 'M'));
             temp = new Date(temp.getFullYear(), temp.getMonth() + 1, 0);
             temp = moment(temp);
+
             line.push( { startDate, temp } );
             
         }else{ 
@@ -141,4 +136,21 @@ export const getMonthlyDate = (start, end) => {
 
     return line;
 
+}
+
+// Get weekly and fortnightly payment dates as an object array
+export const getWeeklyPaymentDates = (start, end, frequency) => {
+    let line      = [];
+    let end_date  = moment(end);
+    let numberOfDays ;
+    for (let index = moment(start) ; index.isBefore(end_date); index.add(numberOfDays + 1, 'd')) {
+        numberOfDays  = getFrequencyData(frequency, index);
+        let startDate = moment(index);
+        let temp      = moment(startDate).add(numberOfDays, 'd');
+
+        line.push( { startDate, temp } );
+        numberOfDays = getFrequencyData(frequency, index);
+    }
+
+    return line;
 }
