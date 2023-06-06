fffb9cde156e7a3e9b7d019919e76878f760f973
anjulard <ridmianjula@gmail.com>
2023-06-06 20:25:30 +0530

    Modified Add Lease form and Lease Information Page

--- frontend/src/components/ShowLeases.js
+++ frontend/src/components/ShowLeases.js
@@ -1,37 +1,51 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from 'react';
 
 const ShowLeases = () => {
-    const [leases, setLeases] = useState([]);
+  const [leases, setLeases] = useState([]);
 
-    useEffect(() => {
-        getLease();
-    }, []);
+  useEffect(() => {
+    fetchLeases();
+  }, []);
 
-    const getLease = async () => {
-        let result = await fetch('http://localhost:4000/lease/',{
-            method: "GET",
-            mode: 'no-cors'
-        })
-
-        setLeases(result);
+  const fetchLeases = async () => {
+    try {
+      const response = await fetch('http://localhost:4000/lease/'); 
+      const data = await response.json();
+      setLeases(data);
+    } catch (error) {
+      console.error('Error fetching leases:', error);
     }
+  };
 
-    console.warn(leases);
-
-    return (
-        <div className="lease-list">
-            <h1>Lease List</h1>
-            <ul>
-                <li>Lease Id</li>
-                <li>Start Date</li>
-                <li>End Date</li>
-                <li>Frequency</li>
-                <li>Weekly Rent</li>
-                <li>Timezone</li>
-            </ul>
-        </div>
-    )
-
-}
+  return (
+    <div>
+      <h1 className="lease">Lease Information</h1>
+        <table className="lease-list">
+        <thead className="lease-list">
+            <tr>
+            <th>Lease Id</th>
+            <th>Start Date</th>
+            <th>End Date</th>
+            <th>Frequency</th>
+            <th>Weekly Rent</th>
+            <th>Timezone</th>
+            </tr>
+        </thead>
+        <tbody>
+            {leases.map((lease) => (
+            <tr key={lease.lease_id}>
+                <td>{lease.lease_id}</td>
+                <td>{lease.start_date}</td>
+                <td>{lease.end_date}</td>
+                <td>{lease.frequency}</td>
+                <td>{lease.weekly_rent}</td>
+                <td>{lease.timezone}</td>
+            </tr>
+            ))}
+        </tbody>
+        </table>
+    </div>
+  );
+};
 
 export default ShowLeases;
